e714c5575fa54022b8716989bec39f6c960834ca
Jorge <jorge.luna@utexas.edu>
2025-01-03 22:30:05 +0000

    adding docs

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# c2celite101-python
+# C2C Elite 101 Python Chatbot Starter
+
+This project is a starter project that includes a dev container that is set up for a python
+development environment.
+
+The project is meant to be a starter for your chatbot project.
+
+Fork this project and start your chatbot creation journey!
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+
+"""
+    Welcome to Elite 101 this program is a starter for your chatbot project.
+    The starter prompts the user to enter their name and then greets them with a personalized message.
+
+    Functions:
+        get_user_name(): Prompts the user to enter their name and returns it.
+        greet_user(name): Prints a greeting message using the provided name.
+        main(): Main function that orchestrates the user input and greeting process.
+
+    Execution:
+        When the script is run directly (not imported as a module), it will execute the main() function.
+"""
+
+
 def get_user_name():
     return input("Please enter your name: ")
 
