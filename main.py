81042e286271f0a8a5b0822a6c4429c6879fb77a
vsm-777 <vsmandha10@gmail.com>
2025-09-18 22:24:09 +0000

    Chatbot prototype

--- main.py
+++ main.py
@@ -14,14 +14,41 @@
 
 
 def get_user_name():
+    print("Welcome to our store's chatbot! ")
     return input("Please enter your name: ")
 
 def greet_user(name):
     print(f"Hello, {name}, how are you?")
+    return input(f"How old are you, {name}? ")
+
+def options(name):
+    print(f"How can I help you {name}?")
+    print("1. Placeholder option 1")
+    print("2. Placeholder option 2")
+    print("3. Placeholder option 3")
+    print("4. Exit the conversation")
+    print()
+    return input("Which option would you like to choose? ")
+
+def user_choice(choice):
+    while choice != '4':
+        if choice == '1':
+            print("You selected option 1.")
+        elif choice == '2':
+            print("You selected option 2.")
+        elif choice == '3':
+            print("You selected option 3.")
+        elif choice == '4':
+            print("Exiting the conversation. Goodbye!")
+        else:
+            print("Invalid choice. Please select a valid option.")
+
+
 
 def main():
     user_name = get_user_name()
-    greet_user(user_name)
+    age=greet_user(user_name)
+    choice=options(user_name)
 
 if __name__ == "__main__":
     main()
